1eef6737c508d59d8ec1f498bbbc971ea1d777b6
Aleksandr Bohdanov <57062636+bohdanov90@users.noreply.github.com>
2020-03-30 09:14:52 +0300

    projects' description updated

--- scripts/data.js
+++ scripts/data.js
@@ -2,7 +2,7 @@ var data = [
 	{
 		id: 1,
 		title: "To-Do List",
-		description: "Simple To-Do app made with Angular2+ Framework and TypeScript. Will be useful for grocery lists or simple every day tasks. Thanks to Local Storage you can close browser and retrieve data later without losses.",
+		description: "Simple To-Do List created using Angular2+ and TypeScript. Can be used for grocery lists or simple every day tasks. Saves your data to Local Storage.",
 		linkToDemo: "https://bohdanov90.github.io/ToDoList/",
 		linkToCode: "https://github.com/bohdanov90/ToDoList/tree/no-build",
 		image: "images/works-screen/to-do-list.jpg",
@@ -11,43 +11,43 @@ var data = [
 	{
 		id: 2,
 		title: "Food Calculator",
-		description: "Find out how many calories you consume with my app. Search food database for items you have consumed, choose amount of servings and add them to your totals. App will calculate total calories you consumed today. Built with pure JavaScript and AJAX technique. BEM methodology and SCSS preprocessor are used. Adapted for different devices.",
+		description: "Search database for food and drinks you have consumed and choose amount of servings. App will calculate total calories you consumed today. Created using pure JavaScript, AJAX, BEM and SCSS. Adapted for different devices.",
 		linkToDemo: "https://bohdanov90.github.io/Food-Calculator/",
 		linkToCode: "https://github.com/bohdanov90/Food-Calculator",
 		image: "images/works-screen/food-calculator.jpg",
-		tags: ["BEM", "SCSS", "Adaptive Design", "JavaScript", "AJAX"]
+		tags: ["AJAX", "JavaScript", "Adaptive Design", "BEM", "SCSS"]
 	},
 	{
 		id: 3,
 		title: "Geography Trivia",
-		description: "Trivia for all geography lovers with quite difficult questions. Built with pure JavaScript, adapted for different devices, BEM methodology and SCSS preprocessor used.",
+		description: "Trivia game created using pure JavaScript, BEM methodology and SCSS preprocessor, adapted for different devices",
 		linkToDemo: "https://bohdanov90.github.io/Geography-Trivia/",
 		linkToCode: "https://github.com/bohdanov90/Geography-Trivia",
 		image: "images/works-screen/geography-trivia.jpg",
-		tags: ["BEM", "SCSS", "Adaptive Design", "JavaScript"]
+		tags: ["JavaScript", "Adaptive Design", "BEM", "SCSS"]
 	},
 	{
 		id: 4,
 		title: "Pig Game",
-		description: "JavaScript game for two players where you should beat your opponent by reaching Total Score faster. Several breakpoints added so game could be played on different devices. Coded using BEM methodology and SCSS preprocessor.",
+		description: "JavaScript game for two players in which you should defeat your opponent by reaching Total Score faster. Game is adapted for different screens and devices. Created using BEM methodology and SCSS preprocessor.",
 		linkToDemo: "https://bohdanov90.github.io/Pig-Game/",
 		linkToCode: "https://github.com/bohdanov90/Pig-Game",
 		image: "images/works-screen/pig-game.jpg",
-		tags: ["BEM", "SCSS", "Adaptive Design", "JavaScript"]
+		tags: ["JavaScript", "Adaptive Design", "BEM", "SCSS"]
 	},
 	{
 		id: 5,
 		title: "Lian",
-		description: "PSD template converted into HTML using Pixel Perfect approach, BEM methodology and SCSS preprocessor.",
+		description: "PSD template converted into HTML5/CSS3 using Pixel Perfect approach, BEM methodology and SCSS preprocessor.",
 		linkToDemo: "https://bohdanov90.github.io/Lian/",
 		linkToCode: "https://github.com/bohdanov90/Lian",
 		image: "images/works-screen/lian.jpg",
-		tags: ["BEM", "PSD to HTML", "Pixel Perfect", "SCSS"]
+		tags: ["BEM", "SCSS", "Pixel Perfect", "PSD to HTML"]
 	},
 	{
 		id: 6,
 		title: "Whitesquare",
-		description: "PSD template converted into HTML using HTML5 and CSS3.",
+		description: "PSD template converted into HTML5/CSS3 using HTML5 and CSS3.",
 		linkToDemo: "https://bohdanov90.github.io/Whitesquare/",
 		linkToCode: "https://github.com/bohdanov90/Whitesquare",
 		image: "images/works-screen/whitesquare.jpg",
