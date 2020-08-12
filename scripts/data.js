1f62d9669bcfc5463e316305b5409b7d1541a2af
bohdanov90 <aleksandr.bohdanov@gmail.com>
2020-08-12 11:01:37 +0300

    feat: stopwatch added

---
+++ scripts/data.js
@@ -0,0 +1,83 @@
+var data = [
+	{
+		id: 9,
+		title: "weatherApp",
+		description: "Single Page App that fetches weather from any city of the world. Users should authenticate before accessing the weather page. Back end emulated using local storage. Created using RxJS, Angular2+, TypeScript, and Bootstrap.",
+		linkToDemo: "https://bohdanov90.github.io/weatherApp/",
+		linkToCode: "https://github.com/bohdanov90/weatherApp",
+		image: "images/works-screen/weatherApp.jpg",
+		tags: ["RxJS", "Angular 9", "TypeScript", "Bootstrap"]
+	},
+	{
+		id: 8,
+		title: "Cocktail-DB",
+		description: "Single Page App that fetches drinks from the cocktail database and displays them with selected filters to the user. Created using RxJS, Angular2+, and TypeScript.",
+		linkToDemo: "https://bohdanov90.github.io/Cocktail-DB/",
+		linkToCode: "https://github.com/bohdanov90/Cocktail-DB",
+		image: "images/works-screen/cocktail-DB.jpg",
+		tags: ["RxJS", "Angular 9", "TypeScript", "PSD to HTML"]
+	},
+	{
+		id: 7,
+		title: "StopWatch",
+		description: "Minimalistic stopwatch with buttons. 'Start/Stop' - starts / stops + resets / continues timer if 'Wait' button was pressed, 'Wait' - pauses timer, works by double click. 'Reset' - resets and starts timer over.",
+		linkToDemo: "https://bohdanov90.github.io/StopWatch/",
+		linkToCode: "https://github.com/bohdanov90/StopWatch",
+		image: "images/works-screen/stop-watch.jpg",
+		tags: ["RxJS", "Angular 9", "TypeScript"]
+	},
+	{
+		id: 6,
+		title: "To-Do List",
+		description: "Simple To-Do List created using Angular2+ and TypeScript. It can be used for grocery lists or simple everyday tasks. Saves your data to Local Storage.",
+		linkToDemo: "https://bohdanov90.github.io/ToDoList/",
+		linkToCode: "https://github.com/bohdanov90/ToDoList/tree/no-build",
+		image: "images/works-screen/to-do-list.jpg",
+		tags: ["Angular 9", "TypeScript", "Adaptive Design"]
+	},
+	{
+		id: 5,
+		title: "Food Calculator",
+		description: "Search database for food and drinks you have consumed and choose the amount of servings. The App will calculate the total calories you consumed today. Created using pure JavaScript, AJAX, BEM, and SCSS. Adapted for different devices.",
+		linkToDemo: "https://bohdanov90.github.io/Food-Calculator/",
+		linkToCode: "https://github.com/bohdanov90/Food-Calculator",
+		image: "images/works-screen/food-calculator.jpg",
+		tags: ["AJAX", "JavaScript", "Adaptive Design", "BEM", "SCSS"]
+	},
+	{
+		id: 4,
+		title: "Geography Trivia",
+		description: "Trivia game created using pure JavaScript, BEM methodology, and SCSS preprocessor, adapted for different devices.",
+		linkToDemo: "https://bohdanov90.github.io/Geography-Trivia/",
+		linkToCode: "https://github.com/bohdanov90/Geography-Trivia",
+		image: "images/works-screen/geography-trivia.jpg",
+		tags: ["JavaScript", "Adaptive Design", "BEM", "SCSS"]
+	},
+	{
+		id: 3,
+		title: "Pig Game",
+		description: "JavaScript game for two players in which you should defeat your opponent by reaching Total Score faster. Game is adapted for different screens and devices. Created using BEM methodology and SCSS preprocessor.",
+		linkToDemo: "https://bohdanov90.github.io/Pig-Game/",
+		linkToCode: "https://github.com/bohdanov90/Pig-Game",
+		image: "images/works-screen/pig-game.jpg",
+		tags: ["JavaScript", "Adaptive Design", "BEM", "SCSS"]
+	},
+	{
+		id: 2,
+		title: "Lian",
+		description: "PSD template converted into HTML5/CSS3 using the Pixel Perfect approach, BEM methodology, and SCSS preprocessor.",
+		linkToDemo: "https://bohdanov90.github.io/Lian/",
+		linkToCode: "https://github.com/bohdanov90/Lian",
+		image: "images/works-screen/lian.jpg",
+		tags: ["BEM", "SCSS", "Pixel Perfect", "PSD to HTML"]
+	},
+	{
+		id: 1,
+		title: "Whitesquare",
+		description: "PSD template converted into HTML5/CSS3 using HTML5 and CSS3.",
+		linkToDemo: "https://bohdanov90.github.io/Whitesquare/",
+		linkToCode: "https://github.com/bohdanov90/Whitesquare",
+		image: "images/works-screen/whitesquare.jpg",
+		tags: ["PSD to HTML"]
+	}
+];
